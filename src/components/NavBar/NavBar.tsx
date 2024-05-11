265de313c5d05dcd623ef2ca0e55beb6c3ae59bf
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-05-11 12:25:54 -0400

    [fix] NavLink

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,4 +1,5 @@
 import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap'
+import { NavLink } from 'react-router-dom'
 
 
 const NavBar = () => {
@@ -10,8 +11,8 @@ const NavBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <NavLink to="#home">Home</NavLink>
+            <NavLink to="#link">Link</NavLink>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
