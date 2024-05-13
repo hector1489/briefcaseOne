e5f4d8ae6f4c060c4afb5f6884ed63e5d3e6d838
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-05-13 11:20:19 -0400

    [fix] viewsLogin

---
+++ src/views/LoginPage/LoginPage.tsx
@@ -0,0 +1,15 @@
+import { Login, Signup } from "../../components"
+
+
+const LoginPage = () => {
+
+    return(
+        <>
+        <Login />
+        <Signup />
+        </>
+    )
+}
+
+
+export default LoginPage
