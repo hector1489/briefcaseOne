6de870a0c4dccea79b5edec6ca10de00baf45529
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-05-21 14:28:27 -0400

    [fix] css navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,24 +1,27 @@
 import { Navbar, Container, Nav } from 'react-bootstrap'
 import { NavLink } from 'react-router-dom'
+import './NavBar.css'
 
 
 const NavBar = () => {
 
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">Briefcase</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto gap-4">
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/login">Log/Sign</NavLink>
-            <NavLink to="/profile">Profile</NavLink>
-            <NavLink to="/">News</NavLink>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div className='box-NavBar'>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">Briefcase</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto gap-4">
+              <NavLink to="/">Home</NavLink>
+              <NavLink to="/login">Log/Sign</NavLink>
+              <NavLink to="/profile">Profile</NavLink>
+              <NavLink to="/">News</NavLink>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
   )
 }
 
