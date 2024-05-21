e62fcf71b603d19570f49c62079b4f6d0bfda144
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-05-21 13:17:45 -0400

    [fix] header css

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,13 @@
-
+import './Header.css'
 
 const Header = () => {
 
-    return(
-        <div className="box-Header">
-        </div>
-    )
+  return (
+    <div className="box-Header d-flex text-center">
+      <h2>briefcase free</h2>
+      <p>plataforma para desarrolladores.</p>
+    </div>
+  )
 }
 
 export default Header
