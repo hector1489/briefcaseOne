f51f65077b0cc93fbb1747840903b65655241b1d
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-05-23 11:47:30 -0400

    [fix] context-Downloads

--- src/components/DownloadFile/DownloadFile.tsx
+++ src/components/DownloadFile/DownloadFile.tsx
@@ -3,7 +3,13 @@ import DataContext from '../../context/context'
 import './DownloadFile.css'
 
 const DownloadFile = () => {
-  const { pdfFileUrl } = useContext(DataContext)
+  const context = useContext(DataContext)
+
+  if (!context) {
+    throw new Error("DataContext debe ser usado dentro de un DataProvider")
+  }
+
+  const { pdfFileUrl } = context
 
   return (
     <div className="DownloadFile">
