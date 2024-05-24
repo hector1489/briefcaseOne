ffe6a40838868c1bf02d3479f6cf6fa1171bf507
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-05-24 18:06:09 -0400

    [fix]color text header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,8 +4,8 @@ const Header = () => {
 
   return (
     <div className="box-Header">
-      <h2 className='text-uppercase fw-bold'>briefcase free</h2>
-      <p>Developer Platform.</p>
+      <h2 className='text-uppercase fw-bold'>briefcase <span className='text-info'>free</span></h2>
+      <p>convertidor de archivos.</p>
     </div>
   )
 }
