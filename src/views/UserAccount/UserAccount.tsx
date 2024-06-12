dd008acbd3afb2a811633affc42c1a3e4411b751
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-06-12 14:16:04 -0400

    clean user page

--- src/views/UserAccount/UserAccount.tsx
+++ src/views/UserAccount/UserAccount.tsx
@@ -2,11 +2,11 @@
 
 const UserAccount = () => {
 
-    return (
-        <>
-        <h3>traer el usuario del estado</h3>
-        <p>mostrar</p>
-        </>
-    )
+  return (
+    <>
+      <h3>traer el usuario del estado</h3>
+      <p>mostrar</p>
+    </>
+  )
 }
- export default UserAccount
+export default UserAccount
