4c489800d154f7929e1a674fcca72539a726df63
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-06-12 14:12:51 -0400

    clean News page

--- src/views/News/News.tsx
+++ src/views/News/News.tsx
@@ -2,13 +2,13 @@ import { NavBar, NewsComponent } from "../../components"
 
 const News = () => {
 
-    return(
-        <>
-        <NavBar />
-        <p>vista</p>
-        <NewsComponent />
-        </>
-    )
+  return (
+    <>
+      <NavBar />
+      <p>vista</p>
+      <NewsComponent />
+    </>
+  )
 }
 
 export default News
