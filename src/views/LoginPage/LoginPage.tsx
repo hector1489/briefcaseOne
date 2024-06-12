9bcee416fd2180f1dc1b902f6b20557a7d3861be
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-06-12 14:11:49 -0400

    clean loadin page

--- src/views/LoginPage/LoginPage.tsx
+++ src/views/LoginPage/LoginPage.tsx
@@ -3,15 +3,15 @@ import { Login, NavBar, Signup } from "../../components"
 
 const LoginPage = () => {
 
-    return (
-        <>
-        <NavBar />
-        <div className="LoginPage d-flex flex-row justify-content-center">
-            <Login />
-            <Signup />
-        </div>
-        </>
-    )
+  return (
+    <>
+      <NavBar />
+      <div className="LoginPage d-flex flex-row justify-content-center">
+        <Login />
+        <Signup />
+      </div>
+    </>
+  )
 }
 
 
