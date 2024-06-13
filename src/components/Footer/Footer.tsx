6613559b3878ec8b4f9784b0ca8e2d7a533c14e0
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-06-13 14:36:15 -0400

    clean footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,11 @@
 
 const Footer = () => {
 
-    return (
-        <footer className="text-center text-white">
-            <p>💼 Thanks for visiting <span className="text-info">@</span>BriefCase</p>
-        </footer>
-    )
+  return (
+    <footer className="text-center text-white">
+      <p>💼 Thanks for visiting <span className="text-info">@</span>BriefCase</p>
+    </footer>
+  )
 }
- export default Footer
+export default Footer
 
