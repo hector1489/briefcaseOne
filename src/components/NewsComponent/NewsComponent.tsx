d0667adb36860bccc58a98bc161fea32d625ca82
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-06-13 14:41:46 -0400

    clean news

--- src/components/NewsComponent/NewsComponent.tsx
+++ src/components/NewsComponent/NewsComponent.tsx
@@ -2,12 +2,12 @@
 
 const NewsComponent = () => {
 
-    return(
-        <div className="text-white">
-        <h3>Briefcase</h3>
-        <p>Convertidor de archivos online gratuito.</p>
-        </div>
-    )
+  return (
+    <div className="text-white">
+      <h3>Briefcase</h3>
+      <p>Convertidor de archivos online gratuito.</p>
+    </div>
+  )
 }
 
 export default NewsComponent
